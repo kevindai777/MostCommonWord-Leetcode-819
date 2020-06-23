7630e58de1fb83828bb5a4018679b4f07cd2a7ca
kevindai777 <66235906+kevindai777@users.noreply.github.com>
2020-06-23 15:01:35 -0400

    edits to solution and explanation

--- mostcommonword.js
+++ mostcommonword.js
@@ -8,9 +8,11 @@ let banned  = ['hit']
 //O(n) solution that uses regex as well as a hashmap to keep track of
 //the frequencies of the words, as well as to remove any punctuation.
 
+//Regex to get rid of any non-alphanumeric characters
 let space = paragraph.toLowerCase().split(/\W+/g)
 let map = {}
 
+//Mapping the frequencies of each word
 for (let word of space) {
     if (map[word] === undefined) {
         map[word] = 1
@@ -23,5 +25,6 @@ let sorted = Object.keys(map).sort((a,b) => {
     return map[b] - map[a]
 })
 
+//Get rid of any banned words
 let filter = sorted.filter(word => !banned.includes(word))
-console.log(filter[0])
+return filter[0]
